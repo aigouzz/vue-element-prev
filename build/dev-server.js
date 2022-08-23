7754b2fd12ee4a50a5c6b33697d21f85282484c3
aigouzz <1003823149@qq.com>
2022-08-23 11:19:00 +0800

    修改本地server的部分文件

--- build/dev-server.js
+++ build/dev-server.js
@@ -7,7 +7,6 @@ var opn = require('opn')
 var proxyMiddleware = require('http-proxy-middleware')
 var webpackConfig = require('./webpack.dev.conf')
 
-console.log(process.env)
 
 // default port where dev server listens for incoming traffic
 var port = process.env.PORT || config.dev.port
